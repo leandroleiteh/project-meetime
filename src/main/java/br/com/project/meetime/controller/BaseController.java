d80912253ba11062d810390f104da01f4c762e03
leandro.leite <leandro.leite@br.experian.com>
2025-04-04 01:20:58 -0300

    feat: base controller

---
+++ src/main/java/br/com/project/meetime/controller/BaseController.java
@@ -0,0 +1,8 @@
+package br.com.project.meetime.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("${base.path}")
+public abstract class BaseController {
+	
+}
