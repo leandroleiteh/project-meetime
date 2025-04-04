f8783312646f4e17462bb547b4bc789d7710bfdc
leandro.leite <leandro.leite@br.experian.com>
2025-04-04 17:39:59 -0300

    test: receive data of webhook contact create v2

--- src/main/java/br/com/project/meetime/controller/WebHookHubspotController.java
+++ src/main/java/br/com/project/meetime/controller/WebHookHubspotController.java
@@ -5,7 +5,6 @@ import br.com.project.meetime.enums.ActionEnum;
 import br.com.project.meetime.enums.HubspotEndpointEnum;
 import br.com.project.meetime.enums.StatusRequestEnum;
 import br.com.project.meetime.util.LogUtil;
-import br.com.project.rest.v1.api.WebhooksApi;
 import br.com.project.rest.v1.model.WebhookHubspotCreateContactRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -16,30 +15,8 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @Log4j2
-public class WebHookHubspotController extends BaseController implements WebhooksApi {
+public class WebHookHubspotController extends BaseController {
 
-//    @CrossOrigin(origins = "*", methods = RequestMethod.POST)
-//    @PostMapping("/webhook/contact-creation")
-//    @Override
-//    public ResponseEntity<Void> handleWebhook(@RequestBody final WebhookHubspotCreateContactRequest contactRequest) {
-//
-//        long initExecutionTime = System.currentTimeMillis();
-//        log.info(LogUtil.buildMessage("<< [INIT REQUEST][WEBHOOK HUBSPOT] PROCESSING >>")
-//                .with(ObservabilityTagConstant.Info.PAGE, HubspotEndpointEnum.WEBHOOK_CONTACT_CREATION.getPath())
-//                .with(ObservabilityTagConstant.Info.ACTION, ActionEnum.PROCESSING));
-//
-//        printWebhookContacts(contactRequest);
-//
-//        log.info(LogUtil.buildMessage("<< [END REQUEST][WEBHOOK HUBSPOT] PROCESSING, SUCCESSFUL >>")
-//                .with(ObservabilityTagConstant.Info.PAGE, HubspotEndpointEnum.WEBHOOK_CONTACT_CREATION.getPath())
-//                .with(ObservabilityTagConstant.Info.ACTION, ActionEnum.PROCESSING)
-//                .with(ObservabilityTagConstant.Info.STATUS_REQUEST, StatusRequestEnum.SUCCESS)
-//                .with(ObservabilityTagConstant.Info.RESPONSE_TIME,
-//                        (System.currentTimeMillis() - initExecutionTime)));
-//
-//        return ResponseEntity.status(HttpStatus.CREATED).build();
-//    }
-//
     @CrossOrigin(origins = "*", methods = RequestMethod.POST)
     @PostMapping("/webhook/contact-creation")
     public ResponseEntity<Void> handleWebhook(@RequestBody final String contactRequest) {
@@ -49,7 +26,7 @@ public class WebHookHubspotController extends BaseController implements Webhooks
                 .with(ObservabilityTagConstant.Info.PAGE, HubspotEndpointEnum.WEBHOOK_CONTACT_CREATION.getPath())
                 .with(ObservabilityTagConstant.Info.ACTION, ActionEnum.PROCESSING));
 
-        log.info(contactRequest.toString());
+        System.out.println(contactRequest.toString());
 
         log.info(LogUtil.buildMessage("<< [END REQUEST][WEBHOOK HUBSPOT] PROCESSING, SUCCESSFUL >>")
                 .with(ObservabilityTagConstant.Info.PAGE, HubspotEndpointEnum.WEBHOOK_CONTACT_CREATION.getPath())
